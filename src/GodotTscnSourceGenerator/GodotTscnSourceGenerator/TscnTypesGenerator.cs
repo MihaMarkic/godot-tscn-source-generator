42b57a8e7ef23f3ec9277f1dfa75065f2d38c54c
Miha Markič <miha@rthand.com>
2023-02-25 19:20:54 +0100

    Implements test generator

---
+++ src/GodotTscnSourceGenerator/GodotTscnSourceGenerator/TscnTypesGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GodotTscnSourceGenerator
+{
+    [Generator]
+    public class TscnTypesGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            context.AddSource("Player.g.cs", "// YEA HELLO");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
